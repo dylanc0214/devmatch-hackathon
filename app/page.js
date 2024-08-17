1a398ee7397f3ad02976b99c1a9c62cc7c8f773d
Dylan <69462123+dylanc0214@users.noreply.github.com>
2024-08-17 04:58:24 +0000

    update

--- app/page.js
+++ app/page.js
@@ -165,7 +165,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       <h1 className="font-bold text-2xl uppercase text-center">
-        Maschain API Workshop Demo
+          Digital Identity Card (DIC)
       </h1>
       <p className="text-sm text-gray-500 lowercase font-normal mt-4">
         {walletAddress ? (
